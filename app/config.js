5733dbe0d79735663cbdf03aa9fa0b8d8f6533c4
Alyssa <alyssaquek@gmail.com>
2014-08-13 00:40:42 +0800

    Update dependencies

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
   db: {
     sess_interval: 3600,
-    uri: 'mongodb://alyssa:nugitrocks123@dbh16.mongolab.com:27167/nuglord-staging' ||
+    uri: //'mongodb://<username>:<password>@dbh16.mongolab.com:27167/<dbname>' ||
       process.env.MONGOLAB_URI ||
       process.env.MONGOHQ_URL ||
       'mongodb://127.0.0.1:27017/test'
